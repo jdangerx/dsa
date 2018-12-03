7b464cbb8f8380da4819481b243ba197301e2411
John Xia <john.danger.xia@gmail.com>
2018-12-03 16:29:27 -0500

    first commit

---
+++ foo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    puts("count 1 - 10:");
+
+    for (int i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+
+    puts("done");
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+dsa
+--
+
+data structures and algorithms scratchpad
+
+reading from [yale cs223](http://www.cs.yale.edu/homes/aspnes/classes/223/notes.html)
